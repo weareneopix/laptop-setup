5342242b8f243c6c600d508bb107f0a3a6365e16
Marko Ilic <ilicmarko@users.noreply.github.com>
2022-03-18 16:20:36 +0100

    Add permissions to n

--- start.sh
+++ start.sh
@@ -44,6 +44,15 @@ brew install --cask \
     zoom
 
 
+# make cache folder (if missing) and take ownership
+sudo mkdir -p /usr/local/n
+sudo chown -R $(whoami) /usr/local/n
+# make sure the required folders exist (safe to execute even if they already exist)
+sudo mkdir -p /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+# take ownership of Node.js install destination folders
+sudo chown -R $(whoami) /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+
+
 fancy_echo "Vim Setup ..."
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
